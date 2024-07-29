eb035311367d504c5cc281ad49183082e198b26b
7A9Oo <hakougn@gmail.com>
2024-07-29 13:03:39 +0200

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "primary-dark-cyan":"hsl(158, 36%, 37%",
+        "primary-cream":"hsl(30, 38%, 92%)",
+        "neutral-very-dark-blue":"hsl(212, 21%, 14%)",
+        "neutral-dark-grayish-blue":"hsl(228, 12%, 48%)"
+      },fontFamily:{
+        "sans":["Montserrat"],
+        "serif":["Fraunces"]
+      }
+    },
+  },
+  plugins: [],
+}
+
