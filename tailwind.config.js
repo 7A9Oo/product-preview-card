8654a201c0d26ba545b3a388314887cc346c87ae
7A9Oo <med.ahk.2018@gmail.com>
2024-09-27 20:57:49 -0700

    continue dev

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        "primary-dark-cyan":"hsl(158, 36%, 37%",
+        "primary-dark-cyan":"hsl(158, 36%, 37%)",
         "primary-cream":"hsl(30, 38%, 92%)",
         "neutral-very-dark-blue":"hsl(212, 21%, 14%)",
         "neutral-dark-grayish-blue":"hsl(228, 12%, 48%)"
